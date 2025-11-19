2739be60d18a2e38ed33ac1b0f310845bf12a3db
Gian Peña <flaconumero2@gmail.com>
2025-11-18 21:33:59 -0500

    add cache to typeracer endpoint
    scraping typeracerdata has added considerable overhead which is making loading times egregious, so this will be the workaround

--- proxy.js
+++ proxy.js
@@ -5,6 +5,8 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 const app = express();
 
+let typeracer_stats = null;
+
 let allowedOrigins = [
    'http://localhost:5173',
    'https://gianpena.xyz'
@@ -39,30 +41,10 @@ app.get('/monkeytype/:time', async (req, res) => {
 });
 
 app.get('/typeracer', async (req, res) => {
-   try {
-      const typeracer_response = await fetch('https://www.typeracerdata.com/api/profile?username=gianthetaco');
-      const typeracer_json = await typeracer_response.json();
-      const stats = `${Math.round(typeracer_json.account.wpm_textbests * 100) / 100}`;
-      const link = `https://data.typeracer.com/pit/profile?user=${typeracer_json.account.username}`;
-
-      const tr_rank_response = await fetch('https://www.typeracerdata.com/leaders?min_races=1500&min_texts=1000&sort=wpm_textbests&rank_start=1&rank_end=1000');
-      const tr_rank_html = await tr_rank_response.text();
-      const $ = cheerio.load(tr_rank_html);
-      const rows = $('table tr');
-      let rank = null;
-      rows.each((index, row) => {
-        const link = $(row).find('a[href*="username=gianthetaco"]');
-
-        if (link.length > 0) {
-          const cells = $(row).find('td');
-          rank = $(cells[0]).text().replace('.', '');
-        }
-      });
-
-      return res.status(200).json({ stats: `${stats} (rank ${rank})`, link });
-   } catch (error) {
-       console.error('TypeRacer API Error:', error);
-       return res.status(500).json({ success: false, error: 'Failed to fetch TypeRacer data' });
+   if (typeracer_stats) {
+     return res.status(200).json(typeracer_stats);
+   } else {
+     return res.status(503).json({ success: false, error: 'TypeRacer stats not available yet' });
    }
 });
 
@@ -98,4 +80,33 @@ app.listen(serverPort, () => {
 	console.log(`Listening on port ${serverPort}...`);
     console.log('Allowed origins are:');
     console.log(allowedOrigins.join('\n'));
+
+  
+  setInterval(async () => {
+    try {
+      const typeracer_response = await fetch('https://www.typeracerdata.com/api/profile?username=gianthetaco');
+      const typeracer_json = await typeracer_response.json();
+      const stats = `${Math.round(typeracer_json.account.wpm_textbests * 100) / 100}`;
+      const link = `https://data.typeracer.com/pit/profile?user=${typeracer_json.account.username}`;
+
+      const tr_rank_response = await fetch('https://www.typeracerdata.com/leaders?min_races=1500&min_texts=1000&sort=wpm_textbests&rank_start=1&rank_end=1000');
+      const tr_rank_html = await tr_rank_response.text();
+      const $ = cheerio.load(tr_rank_html);
+      const rows = $('table tr');
+      let rank = null;
+      rows.each((index, row) => {
+        const link = $(row).find('a[href*="username=gianthetaco"]');
+
+        if (link.length > 0) {
+          const cells = $(row).find('td');
+          rank = $(cells[0]).text().replace('.', '');
+        }
+      });
+
+      typeracer_stats = { stats: `${stats} (rank ${rank})`, link };
+   } catch (error) {
+       console.error('TypeRacer API Error:', error);
+       return res.status(500).json({ success: false, error: 'Failed to fetch TypeRacer data' });
+   }
+  }, 60 * 1000);
 });
